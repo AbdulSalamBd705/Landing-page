61b8519c5ab4c0382e5e86ef377564bad44a2f05
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-02-01 01:43:39 +0600

    how to create Navigation bar

--- food_delivery_1/javascript/app.js
+++ food_delivery_1/javascript/app.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll',()=>{
   const header = document.getElementById("header")
   header.classList.toggle("action", window.scrollY > 0);
 })
+
+const navbar_nav =  document.querySelector(".navbar-nav");
+const toggle = document.querySelector(".toggle");
+const item = document.querySelector('nav-item');
+
+toggle.addEventListener('click', ()=>{
+  navbar_nav.classList.toggle("active")
+})
