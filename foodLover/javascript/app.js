5d695190283e43d3d0a6f9bc64c6993f54368543
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2024-02-02 23:17:58 +0600

    compelet navigation bar

---
+++ foodLover/javascript/app.js
@@ -0,0 +1,25 @@
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+// Start Manu Active Button
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+ const navbar_nav = document.querySelector(".navbar-nav");
+ const bars = document.querySelector(".bars");
+
+
+ bars.addEventListener('click', ()=>{
+  navbar_nav.classList.toggle('active')
+ })
+ 
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+// End Manu Active Button
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+// Start Navigation Animation
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+window.addEventListener("scroll", ()=>{
+  let navbar = document.querySelector(".navbar")
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+// End Navigation Animation
+//=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
